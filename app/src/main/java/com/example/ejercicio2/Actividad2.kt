c5ac0f9a69ac3eac5369bf3015f1c915b7ab4e30
VEALASCO <velascoale83@gmail.com>
2024-04-12 13:31:07 -0600

    Segundo intento de commit y push

---
+++ app/src/main/java/com/example/ejercicio2/Actividad2.kt
@@ -0,0 +1,38 @@
+package com.example.ejercicio2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Actividad2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_actividad2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //Primero seleccionamos el botón o lo que sea que cambiará la página
+
+        //Utilizamos findViewById para obtener el elemento mediante su identificador o "id"
+        val btnSiguiente = findViewById<Button>(R.id.button2)
+
+        //Como siguiente paso debemos utilizar un evento, el cuál estára atenteo al momento en el que el elemento sea seleccionado.
+        btnSiguiente.setOnClickListener{
+
+            //Utilizamos la clase "intent" la cual nos ayuda en el intercambio de información entre componentes.
+            //Esta clase nos va a pedir dos parametros, uno es el contexto o el lugar en el que va a funcionar y el otro es el lugar hacia donde va dirigido o intercambiará información
+            val intent = Intent(this, MainActivity::class.java)
+
+            //Iniciamos la clase intent
+            startActivity(intent)
+        }
+    }
+}
